222482b7b2f2a7027f7b4d8c280ad7eb345115ef
Henrik Riskær <42350063+Byttemos@users.noreply.github.com>
2021-11-23 20:01:53 +0100

    Delete init_db.sh

--- init_db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-rm data.db
-DB_NAME="data.db"
-sqlite3 -header -separator ',' $DB_NAME ".import students_data.csv Students"
-sqlite3 -header -separator ',' $DB_NAME ".import city_data.csv Cities"
-sqlite3 -header -separator ',' $DB_NAME ".import registrations_data.csv Registrations"
-sqlite3 -header -separator ',' $DB_NAME ".import Courses_data.csv Courses"
